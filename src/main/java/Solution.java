8389f293d5bb832d01c30acf1656fab2bcd94de7
alek <wait82985@gmail.com>
2024-10-30 17:05:00 +0500

    much improved version 294ms -> 7ms

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,13 +1,48 @@
 class Solution {
     public int minimumMountainRemovals(int[] nums) {
         int res = 0;
-        for(int i = 1; i < nums.length - 1; i++) {
-            int left = lis(i, nums);
-            int right = lds(i, nums);
-            if(left != 0 && right != 0) {
-                res = Math.max(res, 1 + left + right);
+        int n = nums.length;
+        int[] ldsRes = new int[n];
+        lds(nums, ldsRes);
+        int[] tails = new int[n];
+        tails[0] = nums[0];
+        int p = 0;
+        if(tails[p] < nums[1]) {
+            if(ldsRes[1] > 1) {
+                res = Math.max(res, 1 + ldsRes[1]);
             }
 
+        }
+        for(int i = 1; i < n - 1 ; i++) {
+            int cur = nums[i];
+            if(cur > tails[p]) {
+                p++;
+                tails[p] = cur;
+                if(ldsRes[i + 1] > 1 && tails[p] < nums[i + 1]) {
+                    res = Math.max(res, p + 1 + ldsRes[i + 1]);
+                }
+                continue;
+            }
+            int l = 0;
+            int r = p;
+            int mid;
+            int replacePoint = 0;
+            while(l <= r) {
+                mid = l + (r - l) / 2;
+                if(tails[mid] >= cur) {
+                    replacePoint = mid;
+                    r = mid - 1;
+                }else {
+                    l = mid + 1;
+                }
+            }
+            tails[replacePoint] = cur;
+            if(tails[p] < nums[i + 1]) {
+                if(ldsRes[i + 1] > 1) {
+                    res = Math.max(res, p + 1 + ldsRes[i + 1]);
+                }
+
+            }
         }
 
 
@@ -19,7 +54,7 @@ class Solution {
             return 0;
         }
 
-        int  n = nums.length;
+
         int[] tails = new int [end + 1];
         tails[0] = nums[0];
         int p = 0;
@@ -48,20 +83,18 @@ class Solution {
 
         return p;
     }
-    int lds(int start, int[] nums) {
+    int lds(int[] nums, int[] ldsRes) {
         int  n = nums.length;
-        if(start == n) {
-            return 0;
-        }
 
-        int[] tails = new int[n - start];
+        int[] tails = new int[n];
         tails[0] = nums[n - 1];
         int p = 0;
-        for(int i = n - 2; i >= start; i--) {
+        for(int i = n - 2; i > 0; i--) {
             int cur = nums[i];
             if(cur > tails[p]) {
                 p++;
                 tails[p] = cur;
+                ldsRes[i] = p + 1;
                 continue;
             }
             int l = 0;
@@ -78,6 +111,7 @@ class Solution {
                 }
             }
             tails[replacePoint] = cur;
+            ldsRes[i] = p + 1;
         }
 
         return p;
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class SolutionTest {
     @Test
     public void test1() {
-        int[] nums = {2,1,1,5,6,2,3,1};
+        int[] nums = {1,2,1,3,4,4};
 
         int expected = 3;
         int actual = new Solution().minimumMountainRemovals(nums);
@@ -14,9 +14,9 @@ public class SolutionTest {
     //
     @Test
     public void test2() {
-        int[] nums = {9,8,1,7,6,5,4,3,2,1};
+        int[] nums = {1,2,3,4,4,3,2,1};
 
-        int expected = 2;
+        int expected = 1;
         int actual = new Solution().minimumMountainRemovals(nums);
 
         Assert.assertEquals(expected, actual);
